73718c6b3b470a10ce14bcc98a9c00f9ad7c9c8d
Interceptor <darbaz.me@gmail.com>
2024-07-20 15:06:02 +0300

    Add input validation to isEven and getLastDigit functions

--- random/isEven.ts
+++ random/isEven.ts
@@ -1,4 +1,14 @@
+/**
+ * Returns the last digit of a given number.
+ *
+ * @param {number} number - The number from which to extract the last digit.
+ * @throws {TypeError} Throws a TypeError if the input is not a number.
+ * @return {number} The last digit of the given number.
+ */
 const getLastDigit = (number: number): number => {
+  if (typeof number !== "number") {
+    throw new TypeError("Input must be a number");
+  }
   return Math.abs(number) % 10;
 };
 
@@ -10,6 +20,9 @@ const getLastDigit = (number: number): number => {
  * Complexity: O(1) time | O(1) space
  */
 const isEven = (number: number): boolean => {
+  if (typeof number !== "number") {
+    throw new TypeError("Input must be a number");
+  }
   return getLastDigit(number) % 2 === 0;
 };
 
