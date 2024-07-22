d23fc907828be2dee6fbf8880386cf5fc67831df
Interceptor <darbaz.me@gmail.com>
2024-07-22 23:03:49 +0300

    Implement basic LinkedList with push and isEmpty methods

---
+++ linkedList/linkedList.ts
@@ -0,0 +1,55 @@
+interface INode<T> {
+  value: T;
+  next: INode<T> | null;
+}
+
+class LNode<T> implements INode<T> {
+  constructor(public value: T, public next: INode<T> | null = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+interface ILinkedList<T> {
+  head: INode<T> | null;
+  tail: INode<T> | null;
+  size: number;
+
+  isEmpty: () => boolean;
+  push: (value: T) => void;
+}
+
+class LinkedList<T> implements ILinkedList<T> {
+  public head: INode<T> | null = null;
+  public tail: INode<T> | null = null;
+  public size: number = 0;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  isEmpty(): boolean {
+    return this.size === 0;
+  }
+
+  push(value: T): void {
+    const node = new LNode(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail!.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.push("A");
+linkedList.push("B");
+
+console.log(linkedList);
