8fa7a353f0cee0500e4972e8e1bb23dcbb2f4c5d
Interceptor <darbaz.me@gmail.com>
2024-08-25 21:03:09 +0300

    Add `data` property and `get` method to `DynamicArray` class

--- src/arrays/dynamicArray.ts
+++ src/arrays/dynamicArray.ts
@@ -1,5 +1,6 @@
 interface IDynamicArray {
   length: number;
+  data: number[];
   isEmpty(): boolean;
   add(value: number): void;
   get(index: number): number;
@@ -9,15 +10,24 @@ interface IDynamicArray {
 
 class DynamicArray implements IDynamicArray {
   public length: number;
+  public data: number[];
   constructor() {
     this.length = 0;
+    this.data = [];
   }
 
   isEmpty(): boolean {
     return this.length === 0;
   }
+
+  get(index: number): number {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index out of bounds");
+    }
+    return this.data[index];
+  }
 }
 
 const dynamicArray = new DynamicArray();
 
-console.log(dynamicArray.isEmpty());
+console.log(dynamicArray.get(1));
