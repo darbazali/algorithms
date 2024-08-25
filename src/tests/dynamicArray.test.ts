214c07a59e435532b0b1b1c2b59a0879668a933e
Interceptor <darbaz.me@gmail.com>
2024-08-25 21:39:30 +0300

    Add test for adding an element to DynamicArray

--- src/tests/dynamicArray.test.ts
+++ src/tests/dynamicArray.test.ts
@@ -11,4 +11,10 @@ describe("Dynamic Array", () => {
     const arr = new DynamicArray();
     expect(arr.isEmpty()).toBe(true);
   });
+
+  test("Should add an element to the array", () => {
+    const arr = new DynamicArray();
+    arr.add(1);
+    expect(arr.length).toBe(1);
+  });
 });
