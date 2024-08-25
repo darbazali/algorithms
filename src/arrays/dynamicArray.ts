a610f15a8f73c537d245879a32cdd56a4ca9ac6b
Interceptor <darbaz.me@gmail.com>
2024-08-25 21:32:59 +0300

    Implement `DynamicArray` class with constructor in `dynamicArray.ts`

--- src/arrays/dynamicArray.ts
+++ src/arrays/dynamicArray.ts
@@ -8,3 +8,13 @@ interface IDynamicArray {
   remove(index: number): void;
   clear(): void;
 }
+
+export class DynamicArray implements IDynamicArray {
+  public data: number[];
+  public length: number;
+
+  constructor() {
+    this.data = [];
+    this.length = 0;
+  }
+}
