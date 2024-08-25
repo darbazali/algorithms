19e8dc66b40f53d287cc93b89d75f8fe81f84000
Interceptor <darbaz.me@gmail.com>
2024-08-25 21:30:30 +0300

    interface

--- src/arrays/dynamicArray.ts
+++ src/arrays/dynamicArray.ts
@@ -1,62 +1,10 @@
 interface IDynamicArray {
-  length: number;
   data: number[];
+  length: number;
   isEmpty(): boolean;
   add(value: number): void;
   get(index: number): number;
   set(index: number, value: number): void;
   remove(index: number): void;
   clear(): void;
-}
-
-export class DynamicArray implements IDynamicArray {
-  public length: number;
-  public data: number[];
-  constructor() {
-    this.length = 0;
-    this.data = [];
-  }
-
-  isEmpty(): boolean {
-    return this.length === 0;
-  }
-
-  add(value: number): void {
-    this.data.push(value);
-    this.length++;
-  }
-
-  get(index: number): number {
-    if (this.isEmpty()) {
-      return -1;
-    }
-
-    if (index < 0 || index >= this.length) {
-      throw new Error("Index out of bounds");
-    }
-
-    return this.data[index];
-  }
-
-  clear(): void {
-    this.length = 0;
-    this.data = [];
-  }
-
-  set(index: number, value: number): void {
-    if (index < 0 || index >= this.length) {
-      throw new Error("Index out of bounds");
-    }
-
-    this.data[index] = value;
-  }
-
-  remove(index: number): void {
-    if (index < 0 || index >= this.length) {
-      throw new Error("Index out of bounds");
-    }
-
-    this.data.splice(index, 1);
-    this.length--;
-  }
 }
