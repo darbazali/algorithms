96ca22074374dbc2494f53f93810b00ec9fa777e
Interceptor <darbaz.me@gmail.com>
2024-08-27 22:37:43 +0300

    Add initial implementation of `camelize` function in TypeScript

---
+++ src/arrays/camelize.ts
@@ -0,0 +1,11 @@
+/*
+Translate border-left-width to borderLeftWidth
+
+@Examples:
+camelize('background-color') => 'backgroundColor'
+camelize('-webkit-transition') => 'WebkitTransition'
+*/
+
+const camelize = (str: string): string => {
+  return str;
+};
