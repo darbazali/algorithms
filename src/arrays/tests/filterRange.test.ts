a11be8df5de87b4fcff17dfc3187ea831f16fcc1
Interceptor <darbaz.me@gmail.com>
2024-08-27 23:09:13 +0300

    Add test case for filterRange function to validate filtering within a range

--- src/arrays/tests/filterRange.test.ts
+++ src/arrays/tests/filterRange.test.ts
@@ -5,4 +5,8 @@ describe("Testing for filterRange", () => {
   test("filterRange(...) should return an array", () => {
     expect(filterRange([1, 2, 3], 1, 2)).toBeArray();
   });
+
+  test("filterRange([5, 3, 8, 1] should return [3, 1]", () => {
+    expect(filterRange([5, 3, 8, 1], 1, 4)).toEqual([3, 1]);
+  });
 });
