c2beafd0bc32c93f06ced7a9a4af3c58ef59c2ab
Interceptor <darbaz.me@gmail.com>
2024-08-27 22:53:06 +0300

    Refactor `camelize` function to use `capitalizeFirstLetter` helper function for improved readability

--- src/arrays/camelize.ts
+++ src/arrays/camelize.ts
@@ -6,11 +6,12 @@ camelize('background-color') => 'backgroundColor'
 camelize('-webkit-transition') => 'WebkitTransition'
 */
 
+const capitalizeFirstLetter = (word: string): string =>
+  word.charAt(0).toUpperCase() + word.slice(1);
+
 export const camelize = (str: string): string => {
   return str
     .split("-")
-    .map((word, index) =>
-      index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
-    )
+    .map((word, index) => (index === 0 ? word : capitalizeFirstLetter(word)))
     .join("");
 };
