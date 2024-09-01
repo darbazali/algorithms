9669d5ede105e9e542a960ce411c555d52bd97f0
Interceptor <darbaz.me@gmail.com>
2024-09-01 22:08:11 +0300

    Refactor linked list utility functions and add value retrieval logic

--- src/linkedList/linkedList.ts
+++ src/linkedList/linkedList.ts
@@ -1,15 +1,3 @@
-// const fillValues = (head: LNodeInterface<string>, values: string[]): void => {
-//   if (head === null) return;
-//   values.push(head.value);
-//   fillValues(head.next!, values);
-// };
-
-// const getValues = (head: LNodeInterface<string>): string[] => {
-//   const values: string[] = [];
-//   fillValues(head, values);
-//   return values;
-// };
-
 interface ILinkedListNode<T> {
   value: T;
   next: ILinkedListNode<T> | null;
@@ -34,4 +22,16 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-console.log(a);
+const fillValues = (head: ILinkedListNode<string>, values: string[]): void => {
+  if (head === null) return;
+  values.push(head.value);
+  fillValues(head.next!, values);
+};
+
+const getValues = (head: ILinkedListNode<string>): string[] => {
+  const values: string[] = [];
+  fillValues(head, values);
+  return values;
+};
+
+console.log(getValues(a));
