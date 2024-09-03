c08d15e29086d3dcee1b6153f0364d3af8183fe6
Interceptor <darbaz.me@gmail.com>
2024-09-03 22:30:41 +0300

    Refactor: Introduce LinkedList class with constructor initializing head to null

--- src/linkedList/linkedList.js
+++ src/linkedList/linkedList.js
@@ -5,11 +5,8 @@ class Node {
   }
 }
 
-const a = new Node("A")
-const b = new Node("B")
-
-console.log(a, b)
-
-a.next = b
-
-console.log(a)
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+}
