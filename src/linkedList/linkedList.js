7ca72877574315bfa12d28d5261a34220f0490f8
Interceptor <darbaz.me@gmail.com>
2024-09-04 21:56:43 +0300

    Add Node class implementation to linkedList.js with basic instantiation and logging

--- src/linkedList/linkedList.js
+++ src/linkedList/linkedList.js
@@ -3,3 +3,15 @@
 // TODO: implement append method to ls
 // TODO: implement print method to ls
 // TODO: implement contains method to ls
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+const a = new Node("a")
+const b = new Node("b")
+console.log(a)
+console.log(b)
