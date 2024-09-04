b65e22d28182f7e73fb766dd2b23e8bd418ce835
Interceptor <darbaz.me@gmail.com>
2024-09-04 23:19:48 +0300

    Add append method to LinkedList class and test its functionality

--- src/linkedList/linkedList.js
+++ src/linkedList/linkedList.js
@@ -9,33 +9,27 @@ class LinkedList {
   constructor() {
     this.head = null
   }
+
+  append(value) {
+    if (this.head === null) {
+      this.head = new Node(value)
+      return
+    }
+
+    let current = this.head
+    while (current.next !== null) {
+      current = current.next
+    }
+
+    current.next = new Node(value)
+  }
 }
 
-// class Node {
-//   constructor(value) {
-//     this.value = value
-//     this.next = null
-//   }
-// }
-
-// class LinkedList {
-//   constructor() {
-//     this.head = null
-//   }
-
-//   append(value) {
-//     if (this.head === null) {
-//       this.head = new Node(value)
-//       return
-//     }
-
-//     let current = this.head
-//     while (current.next !== null) {
-//       current = current.next
-//     }
-
-//     current.next = new Node(value)
-//   }
+const list = new LinkedList()
+list.append("a")
+list.append("b")
+list.append("c")
+console.log(list.head)
 
 //   print() {
 //     let str = ""
