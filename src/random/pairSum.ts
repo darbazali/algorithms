926ad9ff5fa94adeb33d70d831d4330a9058fdd7
Interceptor <darbaz.me@gmail.com>
2024-09-06 10:46:37 +0300

    Add pairSum function to find indices of two numbers that add up to a target value

---
+++ src/random/pairSum.ts
@@ -0,0 +1,11 @@
+export const pairSum = (numbers: number[], target: number): number[] => {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+
+  return []
+}
