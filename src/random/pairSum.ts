8f0761809282445b75cc6006d34d7c7bfe12a8fe
Interceptor <darbaz.me@gmail.com>
2024-09-07 07:50:33 +0300

    Refactor `pairSum` function to improve efficiency by using a hash map for lookups.

--- src/random/pairSum.ts
+++ src/random/pairSum.ts
@@ -1,23 +1,10 @@
 export const pairSum = (numbers: number[], target: number): number[] => {
-  for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return [i, j]
-      }
-    }
-  }
-
-  return []
-}
-
-export const pairSum2 = (numbers: number[], targetSum: number): number[] => {
   const previousNums: Record<number, number> = {}
 
-  for (let i = 0; i < numbers.length; i += 1) {
+  for (let i = 0; i < numbers.length; i++) {
     const num = numbers[i]
-    const complement = targetSum - num
+    const complement = target - num
     if (complement in previousNums) return [i, previousNums[complement]]
-
     previousNums[num] = i
   }
 
