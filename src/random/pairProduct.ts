3dd82d49a25048e5fa9375d7188456bdde8d1ed7
Interceptor <darbaz.me@gmail.com>
2024-09-07 08:12:32 +0300

    Optimize `pairProduct` function to use a hash map for improved performance.

--- src/random/pairProduct.ts
+++ src/random/pairProduct.ts
@@ -1,3 +1,12 @@
 export const pairProduct = (numbers: number[], targetProduct: number): number[] => {
-  return numbers
+  const previousNums: Record<number, number> = {}
+
+  for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i]
+    const complement = targetProduct / num
+    if (complement in previousNums) return [previousNums[complement], i]
+    previousNums[num] = i
+  }
+
+  return []
 }
