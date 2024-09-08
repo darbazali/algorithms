2e8320a389fb5ec184a0f82982fa65d020168f9b
Interceptor <darbaz.me@gmail.com>
2024-09-08 07:49:49 +0300

    Refactor: Add braces to if statement in pairSum function for readability

--- src/random/pairSum.ts
+++ src/random/pairSum.ts
@@ -15,7 +15,9 @@ export const pairSum = (numbers: number[], target: number): number[] => {
   for (let i = 0; i < numbers.length; i++) {
     const num = numbers[i]
     const complement = target - num
-    if (complement in previousNums) return [previousNums[complement], i]
+    if (complement in previousNums) {
+      return [previousNums[complement], i]
+    }
     previousNums[num] = i
   }
 
