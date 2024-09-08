153ec36bbdf126de3493f602c45f43b115da072f
Interceptor <darbaz.me@gmail.com>
2024-09-08 07:24:11 +0300

    Refactor `anagrams` function to remove dependency on `createCharCounterMap` and use internal character counting logic.

--- src/random/anagrams.ts
+++ src/random/anagrams.ts
@@ -1,20 +1,26 @@
-import { createCharCounterMap } from "../lib"
-
 export const anagrams = (s1: string, s2: string): boolean => {
   if (s1 === s2) return true
 
-  const chars = createCharCounterMap(s1)
+  const count: Record<string, number> = {}
+
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0
+    }
+
+    count[char] += 1
+  }
 
   for (let char of s2) {
-    if (char in chars) {
-      chars[char] -= 1
+    if (char in count) {
+      count[char] -= 1
     } else {
       return false
     }
   }
 
-  for (let char in chars) {
-    if (chars[char] !== 0) {
+  for (let char in count) {
+    if (count[char] !== 0) {
       return false
     }
   }
