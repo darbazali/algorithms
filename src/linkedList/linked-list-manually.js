ad7133f0f55790ca0c1e0c1bee000f8671c27cdc
Interceptor <darbaz.me@gmail.com>
2024-09-10 21:31:05 +0300

    Add mergeLists function to merge two sorted linked lists

--- src/linkedList/linked-list-manually.js
+++ src/linkedList/linked-list-manually.js
@@ -153,3 +153,28 @@ const zipperList = (head1, head2) => {
 
   return head1
 }
+
+const mergeLists = (head1, head2) => {
+  const dummyHead = new Node(null)
+  let tail = dummyHead
+
+  let current1 = head1
+  let current2 = head2
+
+  while (current1 !== null && current2 !== null) {
+    if (current1.value < current2.value) {
+      tail.next = current1
+      current1 = current1.next
+    } else {
+      tail.next = current2
+      current2 = current2.next
+    }
+
+    tail = tail.next
+  }
+
+  if (current1 !== null) tail.next = current1
+  if (current2 !== null) tail.next = current2
+
+  return dummyHead.next
+}
