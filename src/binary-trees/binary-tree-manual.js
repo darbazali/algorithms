749b1026a05e881322e50e705989d512e974925a
Interceptor <darbaz.me@gmail.com>
2024-09-11 20:11:59 +0300

    Add example binary tree construction and visualization in `binary-tree-manual.js`

--- src/binary-trees/binary-tree-manual.js
+++ src/binary-trees/binary-tree-manual.js
@@ -5,3 +5,26 @@ class Node {
     this.left = null
   }
 }
+
+const a = new Node("a")
+const b = new Node("b")
+const c = new Node("c")
+const d = new Node("d")
+const e = new Node("e")
+const f = new Node("f")
+
+a.left = b
+a.right = c
+
+b.left = d
+b.right = e
+
+c.left = f
+
+console.log(a)
+
+//      a
+//     / \
+//    b    c
+//   / \    \
+//  d   e    f
