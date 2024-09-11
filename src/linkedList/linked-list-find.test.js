c51df6b8a2bf851a9c63d0581d1d746012b0f6a8
Interceptor <darbaz.me@gmail.com>
2024-09-11 22:54:29 +0300

    Add tests for linkedListFindRecursive function in linked-list-find.test.js

--- src/linkedList/linked-list-find.test.js
+++ src/linkedList/linked-list-find.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "bun:test"
-import linkedListFind from "./linked-list-find"
+import linkedListFind, { linkedListFindRecursive } from "./linked-list-find"
 import createLinkedList from "./create-linked-list"
 
 describe("linkedListFind", () => {
@@ -15,5 +15,21 @@ describe("linkedListFind", () => {
 
   test('linkedListFind(list, "d") should return false', () => {
     expect(linkedListFind(list, "d")).toBeFalse()
+  })
+})
+
+describe("linkedListFindRecursive", () => {
+  const list = createLinkedList(["a", "b", "c"])
+
+  test('linkedListFindRecursive(null, "c") should return a boolean', () => {
+    expect(linkedListFindRecursive(list, "c")).toBeBoolean()
+  })
+
+  test('linkedListFindRecursive(list, "c") should return true', () => {
+    expect(linkedListFindRecursive(list, "c")).toBeTrue()
+  })
+
+  test('linkedListFindRecursive(list, "d") should return false', () => {
+    expect(linkedListFindRecursive(list, "d")).toBeFalse()
   })
 })
