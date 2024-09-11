bd881406e913f17ae1226036f84d41938f13e9ae
Interceptor <darbaz.me@gmail.com>
2024-09-11 22:31:02 +0300

    Add additional test cases for `linkedListValues` function
    - Added a new linked list `list2` with string values.
    - Added tests to verify `linkedListValues` returns correct arrays for both numeric and string linked lists.

--- src/linkedList/linked-list-values.test.js
+++ src/linkedList/linked-list-values.test.js
@@ -4,7 +4,17 @@ import createLinkedList from "./create-linked-list"
 
 describe("linkedListValues", () => {
   const list = createLinkedList([1, 2, 3, 4])
-  test("linkedListValues should return an array", () => {
+  const list2 = createLinkedList(["a", "b", "c"])
+
+  test("linkedListValues(list) should return an array", () => {
     expect(linkedListValues(list)).toBeArray()
+  })
+
+  test("linkedListValues(list) should return a [1, 2, 3, 4]", () => {
+    expect(linkedListValues(list)).toEqual([1, 2, 3, 4])
+  })
+
+  test('linkedListValues(list2) should return a ["a", "b", "c"]', () => {
+    expect(linkedListValues(list2)).toEqual(["a", "b", "c"])
   })
 })
