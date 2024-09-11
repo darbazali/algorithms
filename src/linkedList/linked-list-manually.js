f03b66cabf15bacae3261e278131c9f46f7bc1f6
Interceptor <darbaz.me@gmail.com>
2024-09-11 05:47:01 +0300

    Add function to sum two linked lists and return the result as a new linked list

--- src/linkedList/linked-list-manually.js
+++ src/linkedList/linked-list-manually.js
@@ -298,5 +298,34 @@ const createLinkedListRecursive = (values, i = 0) => {
   return head
 }
 
-const newList = createLinkedListRecursive([1, 2, 3, 4, 5])
-printLinkedList(newList)
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null)
+  let tail = dummyHead
+
+  let carry = 0
+  let current1 = head1
+  let current2 = head2
+
+  while (current1 !== null || current2 !== null || carry !== 0) {
+    const val1 = current1 === null ? 0 : current1.value
+    const val2 = current2 === null ? 0 : current2.value
+
+    const sum = val1 + val2 + carry
+    carry = sum > 9 ? 1 : 0
+
+    const digit = sum % 10
+    tail.next = new Node(digit)
+    tail = tail.next
+
+    if (current1 !== null) current1 = current1.next
+    if (current2 !== null) current2 = current2.next
+  }
+
+  return dummyHead.next
+}
+
+const list1 = createLinkedList([1, 2, 6])
+const list2 = createLinkedList([4, 5, 3])
+
+const newList = addLists(list1, list2) // 5 -> 7 -> 9
+linkedListToString(newList)
