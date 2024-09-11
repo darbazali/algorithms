26e1a64a2e94f223554961c9c448b08d1d11215a
Interceptor <darbaz.me@gmail.com>
2024-09-11 22:42:44 +0300

    Add tests for sumLinkedListRecursive function in sum-linked-list.test.js

--- src/linkedList/sum-linked-list.test.js
+++ src/linkedList/sum-linked-list.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "bun:test"
-import sumLinkedList from "./sum-linked-list.js"
+import sumLinkedList, { sumLinkedListRecursive } from "./sum-linked-list.js"
 import createLinkedList from "./create-linked-list.js"
 
 describe("sumLinkedList", () => {
@@ -16,5 +16,22 @@ describe("sumLinkedList", () => {
 
   test("sumLinkedList(emptyList) should return 0", () => {
     expect(sumLinkedList(emptyList)).toBe(0)
+  })
+})
+
+describe("sumLinkedListRecursive", () => {
+  const list = createLinkedList([1, 2, 3, 4])
+  const emptyList = createLinkedList([])
+
+  test("sumLinkedListRecursive(list) should return a number", () => {
+    expect(sumLinkedListRecursive(list)).toBeNumber()
+  })
+
+  test("sumLinkedListRecursive(list) should return 10", () => {
+    expect(sumLinkedListRecursive(list)).toBe(10)
+  })
+
+  test("sumLinkedListRecursive(emptyList) should return 0", () => {
+    expect(sumLinkedListRecursive(emptyList)).toBe(0)
   })
 })
