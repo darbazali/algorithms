6c83241233399edd217e5731a894cadbab0c998a
Interceptor <darbaz.me@gmail.com>
2024-09-14 10:39:51 +0300

    Add createLinkedList function to generate linked lists from array values

--- src/linkedList/create-linked-list.js
+++ src/linkedList/create-linked-list.js
@@ -11,3 +11,19 @@ createLinkedList(["h", "e", "y"]); // h -> e -> y
 createLinkedList([1, 7, 1, 8]); // 1 -> 7 -> 1 -> 8
 
 */
+
+import Node from "./linked-list-node.js"
+
+const createLinkedList = (values) => {
+  const dummyHead = new Node(null)
+  let tail = dummyHead
+
+  for (let value of values) {
+    tail.next = new Node(value)
+    tail = tail.next
+  }
+
+  return dummyHead.next
+}
+
+export default createLinkedList
