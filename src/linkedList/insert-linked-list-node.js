7f7e8366a6b29c5475b926fc4831ede4eb663a53
Interceptor <darbaz.me@gmail.com>
2024-09-14 20:45:42 +0300

    Add `insertNode` function to insert a node at a specific index in a linked list

--- src/linkedList/insert-linked-list-node.js
+++ src/linkedList/insert-linked-list-node.js
@@ -26,3 +26,28 @@ c.next = d;
 insertNode(a, 'x', 2); => a -> b -> x -> c -> d
 
 */
+
+const insertNode = (head, value, index) => {
+  if (index === 0) {
+    const newHead = new Node(value)
+    newHead.next = head
+    return newHead
+  }
+
+  let current = head
+  let count = 0
+
+  while (current !== null) {
+    if (count === index - 1) {
+      const next = current.next
+      current.next = new Node(value)
+      current.next.next = next
+    }
+    count += 1
+    current = current.next
+  }
+
+  return head
+}
+
+export default insertNode
