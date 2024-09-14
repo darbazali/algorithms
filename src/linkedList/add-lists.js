9c07aff3512d68e7a35b7492750b8f7a448e404c
Interceptor <darbaz.me@gmail.com>
2024-09-14 21:54:51 +0300

    Add function to sum two linked lists and return the result as a new linked list

--- src/linkedList/add-lists.js
+++ src/linkedList/add-lists.js
@@ -24,3 +24,31 @@ Then, the reversed linked list format of this problem would appear as:
     5 -> 7 -> 9
 
 */
+
+import Node from "./linked-list-node.js"
+
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null)
+  let tail = dummyHead
+
+  let current1 = head1
+  let current2 = head2
+  let carry = 0
+
+  while (current1 !== null || current2 !== null || carry !== 0) {
+    const val1 = current1 === null ? 0 : current1.value
+    const val2 = current2 === null ? 0 : current2.value
+
+    const sum = val1 + val2 + carry
+    carry = sum > 9 ? 1 : 0
+
+    const digit = sum % 10
+    tail.next = new Node(digit)
+    tail = tail.next
+
+    if (current1 !== null) current1 = current1.next
+    if (current2 !== null) current2 = current2.next
+  }
+
+  return dummyHead.next
+}
