786482c0f5278ee3734dcab868417e568e437cd0
Interceptor <darbaz.me@gmail.com>
2024-09-14 15:06:00 +0300

    Add mergeLists function to merge two sorted linked lists

--- src/linkedList/merge-lists.js
+++ src/linkedList/merge-lists.js
@@ -36,3 +36,31 @@ mergeLists(a, q);
 => 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 12 -> 20 -> 25 -> 28 
 
 */
+
+import Node from "./linked-list-node"
+
+const mergeLists = (head1, head2) => {
+  const dummyNode = new Node(null)
+  let tail = dummyNode
+
+  let current1 = head1
+  let current2 = head2
+
+  while (current1 !== null && current2 !== null) {
+    if (current1.value < current2.value) {
+      tail.next = current1
+      current1 = current1.next
+    } else {
+      tail.next = current2
+      current2 = current2.next
+    }
+
+    tail = tail.next
+  }
+
+  //   handle left overs
+  if (current1 !== null) tail.next = current1
+  if (current2 !== null) tail.next = current2
+
+  return dummyNode.next
+}
