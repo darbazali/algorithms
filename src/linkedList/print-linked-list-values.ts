fa96261e44a4e744315e1d37804e532f5454eb6d
Interceptor <darbaz.me@gmail.com>
2024-09-14 09:49:25 +0300

    Refactor `printLinkedListValues` to print linked list as a formatted string and add example usage

--- src/linkedList/print-linked-list-values.ts
+++ src/linkedList/print-linked-list-values.ts
@@ -1,11 +1,27 @@
 import type { INode } from "./linked-list-node"
+import Node from "./linked-list-node"
 
 const printLinkedListValues = <T>(head: INode<T>): void => {
+  let str = ""
+
   let current = head
   while (current !== null) {
-    console.log(current.value)
+    str += current.value + " -> "
     current = current.next!
   }
+
+  console.log(str + "NULL")
 }
 
 export default printLinkedListValues
+
+const a = new Node("a")
+const b = new Node("b")
+const c = new Node("c")
+const d = new Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+printLinkedListValues(a) // a -> b -> c -> d -> NULL
