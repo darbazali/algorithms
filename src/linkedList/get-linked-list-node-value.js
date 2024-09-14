35c6b813c60c3a894a66031d44c6d61c7cc87348
Interceptor <darbaz.me@gmail.com>
2024-09-14 11:45:40 +0300

    Add getNodeValue function to retrieve node value by index in linked list
    - Implemented getNodeValue function to find the value of a node at a specific index in a linked list.
    - Added test cases to verify the functionality of getNodeValue.

--- src/linkedList/get-linked-list-node-value.js
+++ src/linkedList/get-linked-list-node-value.js
@@ -22,3 +22,26 @@ c.next = d;
 getNodeValue(a, 2); // 'c'
 
 */
+
+import createLinkedList from "./create-linked-list.js"
+
+const list = createLinkedList(["a", "b", "c", "d"])
+
+const getNodeValue = (head, target) => {
+  let count = 0
+  let current = head
+
+  while (current !== null) {
+    if (count === target) {
+      return current.value
+    }
+
+    count += 1
+    current = current.next
+  }
+
+  return null
+}
+
+console.log(getNodeValue(list, 2)) // -> 'c'
+console.log(getNodeValue(list, 4)) // -> null
