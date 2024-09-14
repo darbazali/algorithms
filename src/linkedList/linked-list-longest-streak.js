f662ab2776cd89d165443238b48b6adf8a681ca8
Interceptor <darbaz.me@gmail.com>
2024-09-14 20:29:00 +0300

    Add `removeNode` function to delete a node from a linked list
    - Implement `removeNode` function to remove the first instance of a target value from a linked list.
    - Add detailed comments and example usage for `removeNode`.
    - Add section header for `longestStreak` function documentation.

--- src/linkedList/linked-list-longest-streak.js
+++ src/linkedList/linked-list-longest-streak.js
@@ -1,5 +1,7 @@
 /* 
 longest streak
+--------------
+
 Write a function, longestStreak, that takes in the head of a linked list as an argument. 
 The function should return the length of the longest consecutive streak of the same value within the list.
 
