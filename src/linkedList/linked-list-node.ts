87d95bd3c723fdd8ebdb543527ca491f9a64e8a1
Interceptor <darbaz.me@gmail.com>
2024-09-14 09:44:10 +0300

    Refactor: Rename LinkedListNode to Node and update interface definition

--- src/linkedList/linked-list-node.ts
+++ src/linkedList/linked-list-node.ts
@@ -1,15 +1,15 @@
-export interface ILinkedListNode<T> {
-  readonly value: T
-  next: T | null
+export interface INode<T> {
+  value: T
+  next: INode<T> | null
 }
 
-class LinkedListNode<T> implements LinkedListNode<T> {
-  private value: T
-  public next: T | null
+class Node<T> implements INode<T> {
+  public value: T
+  public next: INode<T> | null
   constructor(value: T, next = null) {
     this.value = value
     this.next = next
   }
 }
 
-export default LinkedListNode
+export default Node
