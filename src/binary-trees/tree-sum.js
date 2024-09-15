e1c7cbfaa6d0d141615979a2e54dfdf391caa265
Interceptor <darbaz.me@gmail.com>
2024-09-15 11:51:40 +0300

    Implement depth-first tree sum calculation in `treeSum` function

--- src/binary-trees/tree-sum.js
+++ src/binary-trees/tree-sum.js
@@ -30,3 +30,38 @@ c.right = f;
 treeSum(a); // -> 21
 
 */
+
+import Node from "./binary-tree-node.js"
+
+// depth-first version
+const treeSum = (root) => {
+  if (root === null) return 0
+
+  let sum = 0
+  const stack = [root]
+
+  while (stack.length > 0) {
+    const current = stack.pop()
+    sum += current.value
+
+    if (current.right !== null) stack.push(current.right)
+    if (current.left !== null) stack.push(current.left)
+  }
+
+  return sum
+}
+
+const a = new Node(3)
+const b = new Node(11)
+const c = new Node(4)
+const d = new Node(4)
+const e = new Node(-2)
+const f = new Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+console.log(treeSum(a)) // -> 21
