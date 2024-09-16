996b6a9a719a162a5c086f9120e5285605e015ad
Interceptor <darbaz.me@gmail.com>
2024-09-16 19:36:35 +0300

    Add test cases for `pathFinder` function and import `Node` class in `tree-path-finder.js`

--- src/binary-trees/tree-path-finder.js
+++ src/binary-trees/tree-path-finder.js
@@ -34,6 +34,8 @@ pathFinder(a, 'e'); // -> [ 'a', 'b', 'e' ]
 
 */
 
+import Node from "./binary-tree-node.js"
+
 const pathFinder = (root, target) => {
   if (root === null) return null
   if (root.value === target) return [root.value]
@@ -53,5 +55,28 @@ const pathFinder = (root, target) => {
 
   return null
 }
+
+const root = new Node(0)
+let current = root
+for (let i = 1; i < 5000; i++) {
+  current.right = new Node(i)
+  current = current.right
+}
+
+const a = new Node("a")
+const b = new Node("b")
+const c = new Node("c")
+const d = new Node("d")
+const e = new Node("e")
+const f = new Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+console.log(pathFinder(a, "e")) // -> [ 'a', 'b', 'e' ]
+console.log(pathFinder(root, 4000)) // -> [0, 1, ..., 4000]
 
 export default pathFinder
