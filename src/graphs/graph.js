d8871dd184084547558b6924aea9254e144f6326
Interceptor <darbaz.me@gmail.com>
2024-09-18 13:47:35 +0300

    Add `addEdge` method to Graph class and update example usage
    - Implemented `addEdge` method to create edges between nodes.
    - Updated example usage to demonstrate adding edges between nodes.

--- src/graphs/graph.js
+++ src/graphs/graph.js
@@ -10,11 +10,22 @@ class Graph {
       this.adjacencyList.set(node, [])
     }
   }
+
+  // Add an edge between two vertices (nodes)
+  addEdge = (node1, node2) => {
+    if (this.adjacencyList.has(node1) && this.adjacencyList.has(node2)) {
+      this.adjacencyList.get(node1).push(node2)
+      this.adjacencyList.get(node2).push(node1)
+    }
+  }
 }
 
 const graph = new Graph()
 graph.addNode("a")
 graph.addNode("b")
 graph.addNode("c")
+
+graph.addEdge("a", "b")
+graph.addEdge("a", "c")
 
 console.log(graph.adjacencyList)
