fbc2dd5309776f0b4bf7ee3338e7965e87cc9e42
Interceptor <darbaz.me@gmail.com>
2024-09-18 10:03:41 +0300

    Add Stack class implementation with push, pop, and isEmpty methods

---
+++ src/stack/stack.js
@@ -0,0 +1,22 @@
+class Stack {
+  constructor() {
+    this.items = []
+  }
+
+  push = (item) => {
+    this.items.push(item)
+  }
+
+  pop = () => {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty!")
+    }
+    return this.items.pop()
+  }
+
+  isEmpty = () => this.items.length === 0
+
+  // more methods could be added if needed
+}
+
+export default Stack
