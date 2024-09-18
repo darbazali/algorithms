8c850b82dd9d53ea1386303eb5688e3dd2069610
Interceptor <darbaz.me@gmail.com>
2024-09-18 10:42:49 +0300

    Refactor Queue class to use private field for items and add initial items support in constructor

--- src/queue/queue.js
+++ src/queue/queue.js
@@ -1,20 +1,21 @@
 class Queue {
-  constructor() {
-    this.items = []
+  #items = []
+  constructor(initial) {
+    this.#items = Array.isArray(initial) ? [...initial] : [initial]
   }
 
   enqueue = (item) => {
-    this.items.push(item)
+    this.#items.push(item)
   }
 
   dequeue = () => {
     if (this.isEmpty()) {
       throw new Error("The Queue is empty")
     }
-    return this.items.shift()
+    return this.#items.shift()
   }
 
-  isEmpty = () => this.items.length === 0
+  isEmpty = () => this.#items.length === 0
 }
 
 export default Queue
