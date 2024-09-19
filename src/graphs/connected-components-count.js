a8b6dcb0b7320f02d9b1e6ba5e28f933295e41ca
Interceptor <darbaz.me@gmail.com>
2024-09-19 14:34:09 +0300

    Add function to count connected components in a graph
    - Implement `connectedComponentsCount` function to determine the number of connected components in a graph.
    - Add example usage and console log to demonstrate functionality.

--- src/graphs/connected-components-count.js
+++ src/graphs/connected-components-count.js
@@ -30,3 +30,28 @@ const traverse = (graph, current, visited) => {
 
   return true
 }
+
+const connectedComponentsCount = (graph) => {
+  const visited = new Set()
+  let count = 0
+
+  for (let node in graph) {
+    if (traverse(graph, node, visited) === true) {
+      count += 1
+    }
+  }
+
+  return count
+}
+
+const counts = connectedComponentsCount({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2],
+})
+
+console.log(counts) // -> 2
