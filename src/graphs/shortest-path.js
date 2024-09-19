2b9c4f524a4d91df579a88b8cadbc462dd8396ba
Interceptor <darbaz.me@gmail.com>
2024-09-19 16:19:13 +0300

    Add buildGraph function to construct adjacency list from edge list in shortest-path.js

--- src/graphs/shortest-path.js
+++ src/graphs/shortest-path.js
@@ -20,3 +20,18 @@ const edges = [
 shortestPath(edges, 'w', 'z'); // -> 2
 
 */
+
+const buildGraph = (edges) => {
+  const graph = {}
+
+  for (let edge of edges) {
+    const [a, b] = edge
+    if (!(a in graph)) graph[a] = []
+    if (!(b in graph)) graph[b] = []
+
+    graph[a].push(b)
+    graph[b].push(a)
+  }
+
+  return graph
+}
