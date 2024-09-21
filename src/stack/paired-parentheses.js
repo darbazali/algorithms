6b51f7b17f05e03a025f9f5e0a0057569ebf0055
Interceptor <darbaz.me@gmail.com>
2024-09-21 15:06:10 +0300

    Add documentation for `pairedParentheses` function in `paired-parentheses.js`

---
+++ src/stack/paired-parentheses.js
@@ -0,0 +1,15 @@
+/* 
+paired parentheses
+------------------
+
+Write a function, pairedParentheses, that takes in a string as an argument. 
+The function should return a boolean indicating whether or not the string has well-formed parentheses.
+
+You may assume the string contains only alphabetic characters, '(', or ')'.
+
+@example:
+---------
+
+pairedParentheses("(david)((abby))"); // -> true
+
+*/
