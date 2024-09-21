5a2be088e64809fe6a7caf3a57acbf38c1d38f82
Interceptor <darbaz.me@gmail.com>
2024-09-21 10:48:14 +0300

    Add initial implementation and documentation for `minimumIsland` function in `minimum-island.js`

---
+++ src/graphs/minimum-island.js
@@ -0,0 +1,25 @@
+/* 
+minimum island
+--------------
+
+Write a function, minimumIsland, that takes in a grid containing Ws and Ls. W represents water and L represents land. 
+The function should return the size of the smallest island. 
+An island is a vertically or horizontally connected region of land.
+
+You may assume that the grid contains at least one island.
+
+@example:
+---------
+
+const grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+];
+
+minimumIsland(grid); // -> 2
+
+*/
