754b6d21a3c390afec3c8c8364db1d486f093fdc
Interceptor <darbaz.me@gmail.com>
2024-09-22 21:25:43 +0300

    Add initial implementation of minimumIsland function with grid traversal and logging

--- src/graphs/minimum-island.js
+++ src/graphs/minimum-island.js
@@ -23,3 +23,22 @@ const grid = [
 minimumIsland(grid); // -> 2
 
 */
+
+const minimumIsland = (grid) => {
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      console.log(r, c, grid[r][c])
+    }
+  }
+}
+
+const grid = [
+  ["W", "L", "W", "W", "W"],
+  ["W", "L", "W", "W", "W"],
+  ["W", "W", "W", "L", "W"],
+  ["W", "W", "L", "L", "W"],
+  ["L", "W", "W", "L", "L"],
+  ["L", "L", "W", "W", "W"],
+]
+
+minimumIsland(grid) // -> 2
