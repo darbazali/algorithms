1acd0eeee2b2325acdf6396a458491b4a80bbef1
Interceptor <darbaz.me@gmail.com>
2024-09-23 16:25:51 +0300

    Implement longest path calculation in directed acyclic graph (DAG)

--- src/graphs/longest-path.js
+++ src/graphs/longest-path.js
@@ -17,3 +17,40 @@ const graph = {
 longestPath(graph); // -> 2
 
 */
+
+const traverseDistance = (graph, node, distance) => {
+  if (node in distance) return distance[node]
+
+  let maxLength = 0
+  for (let neighbor of graph[node]) {
+    const attempt = traverseDistance(graph, neighbor, distance)
+    if (attempt > maxLength) maxLength = attempt
+  }
+
+  distance[node] = 1 + maxLength
+  return distance[node]
+}
+
+const longestPath = (graph) => {
+  const distance = {}
+
+  for (let node in graph) {
+    if (graph[node].length === 0) {
+      distance[node] = 0
+    }
+  }
+
+  for (let node in graph) {
+    traverseDistance(graph, node, distance)
+  }
+
+  return Math.max(...Object.values(distance))
+}
+
+const graph = {
+  a: ["c", "b"],
+  b: ["c"],
+  c: [],
+}
+
+console.log(longestPath(graph)) // -> 2
