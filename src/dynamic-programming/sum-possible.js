980270d5554f4835cced780deb7c62d4ab2d9c12
Interceptor <darbaz.me@gmail.com>
2024-09-23 22:19:05 +0300

    Implement memoization in sumPossible function to optimize performance

--- src/dynamic-programming/sum-possible.js
+++ src/dynamic-programming/sum-possible.js
@@ -13,5 +13,25 @@ You may assume that the target amount is non-negative.
 sumPossible(8, [5, 12, 4]); // -> true, 4 + 4
 sumPossible(15, [6, 2, 10, 19]); // -> false
 sumPossible(10, [4, 5, 7]); // -> true
+sumPossible(2017, [4, 2, 10]); // -> false
 
 */
+
+const sumPossible = (amount, numbers, memo = {}) => {
+  if (amount in memo) return memo[amount]
+
+  if (amount < 0) return false
+  if (amount === 0) return true
+
+  for (let num of numbers) {
+    if (sumPossible(amount - num, numbers, memo) === true) {
+      memo[amount] = true
+      return true
+    }
+  }
+
+  memo[amount] = false
+  return false
+}
+
+console.log(sumPossible(2017, [4, 2, 10])) // -> false
