c722682d5df2dc4ecb134353a58b2344baf29ea7
Interceptor <darbaz.me@gmail.com>
2024-09-23 22:09:40 +0300

    Add initial implementation and documentation for `sumPossible` function in dynamic programming module

---
+++ src/dynamic-programming/sum-possible.js
@@ -0,0 +1,17 @@
+/* 
+sum possible
+------------
+Write a function sumPossible that takes in an amount and an array of positive numbers. 
+The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array. 
+You may reuse numbers of the array as many times as necessary.
+
+You may assume that the target amount is non-negative.
+
+@example:
+---------
+
+sumPossible(8, [5, 12, 4]); // -> true, 4 + 4
+sumPossible(15, [6, 2, 10, 19]); // -> false
+sumPossible(10, [4, 5, 7]); // -> true
+
+*/
