4d43b390362c7911fc1a5ea783dfe7cbed8fbb4f
Interceptor <darbaz.me@gmail.com>
2024-09-23 21:17:29 +0300

    Implement memoized tribonacci function and add test cases

--- src/dynamic-programming/tribonacci.js
+++ src/dynamic-programming/tribonacci.js
@@ -19,3 +19,18 @@ tribonacci(5); // -> 4
 tribonacci(37); // -> 1132436852
 
 */
+
+const tribonacci = (n, memo = {}) => {
+  if (n in memo) return memo[n]
+  if (n === 0 || n === 1) return 0
+  if (n === 2) return 1
+
+  memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo)
+  return memo[n]
+}
+
+console.log(tribonacci(0)) // -> 0
+console.log(tribonacci(1)) // -> 0
+console.log(tribonacci(2)) // -> 1
+console.log(tribonacci(5)) // -> 4
+console.log(tribonacci(37)) // -> 1132436852
