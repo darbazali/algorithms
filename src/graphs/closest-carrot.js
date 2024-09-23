c8d37860381875bf649deca2301907296cef30ea
Interceptor <darbaz.me@gmail.com>
2024-09-23 14:57:29 +0300

    Implement `closestCarrot` function to find the shortest path to a carrot in a grid using BFS algorithm.

--- src/graphs/closest-carrot.js
+++ src/graphs/closest-carrot.js
@@ -21,3 +21,56 @@ const grid = [
 closestCarrot(grid, 1, 2); // -> 4
 
 */
+
+const closestCarrot = (grid, startRow, startCol) => {
+  const visited = new Set([startRow + "," + startCol])
+  const queue = [[startRow, startCol, 0]]
+
+  while (queue.length > 0) {
+    const [row, col, distance] = queue.shift()
+
+    if (grid[row][col] === "C") return distance
+
+    const deltas = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ]
+
+    for (let delta of deltas) {
+      const [rowDelta, colDelta] = delta
+
+      const neighborRow = row + rowDelta
+      const neighborCol = col + colDelta
+
+      const rowInbounds = 0 <= neighborRow && neighborRow < grid.length
+      const colInbounds = 0 <= neighborCol && neighborCol < grid[0].length
+
+      const pos = neighborRow + "," + neighborCol
+
+      if (
+        rowInbounds &&
+        colInbounds &&
+        grid[neighborRow][neighborCol] !== "X" &&
+        !visited.has(pos)
+      ) {
+        queue.push([neighborRow, neighborCol, distance + 1])
+        visited.add(pos)
+      }
+    }
+  }
+
+  return -1
+}
+
+const grid = [
+  ["O", "O", "O", "O", "O"],
+  ["O", "X", "O", "O", "O"],
+  ["O", "X", "X", "O", "O"],
+  ["O", "X", "C", "O", "O"],
+  ["O", "X", "X", "O", "O"],
+  ["C", "O", "O", "O", "O"],
+]
+
+console.log(closestCarrot(grid, 1, 2)) // -> 4
