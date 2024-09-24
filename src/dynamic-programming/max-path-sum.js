e11a959c78386b52f0f7fae0d1bd6da187ad7297
Interceptor <darbaz.me@gmail.com>
2024-09-24 16:57:14 +0300

    Add memoization to maxPathSum function to improve performance

--- src/dynamic-programming/max-path-sum.js
+++ src/dynamic-programming/max-path-sum.js
@@ -18,14 +18,18 @@ maxPathSum(grid); // -> 18
 
 */
 
-const maxPathSum = (grid = [], r = 0, c = 0) => {
+const maxPathSum = (grid = [], r = 0, c = 0, memo = {}) => {
+  const pos = r + "," + c
+  if (pos in memo) return memo[pos]
+
   if (r === grid.length || c === grid[0].length) return -Infinity
   if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c]
 
-  const down = maxPathSum(grid, r + 1, c)
-  const right = maxPathSum(grid, r, c + 1)
+  const down = maxPathSum(grid, r + 1, c, memo)
+  const right = maxPathSum(grid, r, c + 1, memo)
 
-  return grid[r][c] + Math.max(down, right)
+  memo[pos] = grid[r][c] + Math.max(down, right)
+  return memo[pos]
 }
 
 const grid = [
@@ -50,4 +54,6 @@ const grid = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
-console.log(maxPathSum(grid)) // -> 82 in 3.659 seconds without memo
+console.log(maxPathSum(grid))
+// -> 82 in 3.659 seconds without memo
+// -> 82 in 0.06  seconds with memo, which is 60 times faster
