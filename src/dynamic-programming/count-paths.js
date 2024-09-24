7b294c25e917d9d0e1cac3c3c4c20d2418ea7304
Interceptor <darbaz.me@gmail.com>
2024-09-24 16:04:31 +0300

    Refactor `countPaths` function to simplify boundary and obstacle checks

--- src/dynamic-programming/count-paths.js
+++ src/dynamic-programming/count-paths.js
@@ -18,17 +18,9 @@ countPaths(grid); // -> 2
 */
 
 const countPaths = (grid = [], r = 0, c = 0) => {
-  // catch out of bounds positions
-  if (r === grid.length) return 0
-  if (c === grid[0].length) return 0
-  if (grid[r][c] === "X") return 0
-
+  if (r === grid.length || c === grid[0].length || grid[r][c] === "X") return 0
   if (r === grid.length - 1 && c === grid[0].length - 1) return 1
-
-  const downCount = countPaths(grid, r + 1, c) // move to down
-  const rightCount = countPaths(grid, r, c + 1) // move to right
-
-  return downCount + rightCount
+  return countPaths(grid, r + 1, c) + countPaths(grid, r, c + 1)
 }
 
 const grid = [
