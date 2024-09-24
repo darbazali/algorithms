4cc9fc6c2f43e77e2bc13ce3c37125af772de928
Interceptor <darbaz.me@gmail.com>
2024-09-24 20:30:53 +0300

    Add initial implementation of nonAdjacentSum function with example usage

---
+++ src/dynamic-programming/non-adjacent-sum.js
@@ -0,0 +1,18 @@
+/* 
+non adjacent sum
+----------------
+Write a function, nonAdjacentSum, that takes in an array of numbers as an argument. 
+The function should return the maximum sum of non-adjacent elements in the array. 
+There is no limit on how many elements can be taken into the sum as long as they are not adjacent.
+
+For example, given:
+[2, 4, 5, 12, 7]
+
+The maximum non-adjacent sum is 16, because 4 + 12. 
+4 and 12 are not adjacent in the array.
+
+@example:
+const nums = [2, 4, 5, 12, 7];
+nonAdjacentSum(nums); // -> 16
+
+*/
