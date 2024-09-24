0baad56d59d115ba05e89de663473010a83ce12d
Interceptor <darbaz.me@gmail.com>
2024-09-24 20:57:20 +0300

    Add memoization to `summingSquares` function to improve performance

--- src/dynamic-programming/summing-squares.js
+++ src/dynamic-programming/summing-squares.js
@@ -20,17 +20,19 @@ summingSquares(8); // -> 2
 
 */
 
-const summingSquares = (n) => {
+const summingSquares = (n, memo = {}) => {
+  if (n in memo) return memo[n]
   if (n === 0) return 0
 
   let minSquares = Infinity
   for (let i = 1; i <= Math.sqrt(n); i++) {
     const square = i * i
-    const numSquares = 1 + summingSquares(n - square)
+    const numSquares = 1 + summingSquares(n - square, memo)
     minSquares = Math.min(minSquares, numSquares)
   }
 
-  return minSquares
+  memo[n] = minSquares
+  return memo[n]
 }
 
-console.log(summingSquares(87)) // -> 4
+console.log(summingSquares(87)) // -> 4 in 0.054 seconds
