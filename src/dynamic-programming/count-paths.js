34b2ac0dac08cc9fbd6bb750fc9d5ade97d32de7
Interceptor <darbaz.me@gmail.com>
2024-09-24 16:03:12 +0300

    Implement `countPaths` function to calculate paths in a grid

--- src/dynamic-programming/count-paths.js
+++ src/dynamic-programming/count-paths.js
@@ -16,3 +16,24 @@ const grid = [
 countPaths(grid); // -> 2
 
 */
+
+const countPaths = (grid = [], r = 0, c = 0) => {
+  // catch out of bounds positions
+  if (r === grid.length) return 0
+  if (c === grid[0].length) return 0
+  if (grid[r][c] === "X") return 0
+
+  if (r === grid.length - 1 && c === grid[0].length - 1) return 1
+
+  const downCount = countPaths(grid, r + 1, c) // move to down
+  const rightCount = countPaths(grid, r, c + 1) // move to right
+
+  return downCount + rightCount
+}
+
+const grid = [
+  ["O", "O"],
+  ["O", "O"],
+]
+
+console.log(countPaths(grid)) // -> 2
