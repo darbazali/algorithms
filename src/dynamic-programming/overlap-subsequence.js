3592d9ccb1fda636a580fb17e3db828b55b2c1ee
Interceptor <darbaz.me@gmail.com>
2024-09-25 11:06:16 +0300

    Implement `overlapSubsequence` function for finding the length of the longest overlapping subsequence between two strings.

--- src/dynamic-programming/overlap-subsequence.js
+++ src/dynamic-programming/overlap-subsequence.js
@@ -11,3 +11,24 @@ while maintaining the relative order of characters.
 overlapSubsequence("dogs", "daogt"); // -> 3
 
 */
+
+const overlapSubsequence = (str1, str2, i = 0, j = 0) => {
+  if (i === str1.length || j === str2.length) return 0
+
+  if (str1[i] === str2[j]) {
+    return 1 + overlapSubsequence(str1, str2, i + 1, j + 1)
+  } else {
+    return Math.max(
+      overlapSubsequence(str1, str2, i + 1, j),
+      overlapSubsequence(str1, str2, i, j + 1),
+    )
+  }
+}
+
+console.log(overlapSubsequence("dogs", "daogt")) // -> 3
+console.log(
+  overlapSubsequence(
+    "mumblecorebeardleggingsauthenticunicorn",
+    "succulentspughumblemeditationlocavore",
+  ),
+) // -> 15
