3cbdf26cc1a8d2df3c573814825e572829e37db3
Interceptor <darbaz.me@gmail.com>
2024-09-25 12:56:51 +0300

    Add `quickestConcat` function to solve the minimum word concatenation problem in dynamic programming module

---
+++ src/dynamic-programming/quickest-concat.js
@@ -0,0 +1,16 @@
+/* 
+quickest concat
+---------------
+Write a function, quickestConcat, that takes in a string and an array of words as arguments. 
+The function should return the minimum number of words needed to build the string by concatenating words of the array. 
+If it is not possible to build the string, then return -1.
+
+You may use words of the array as many times as needed.
+
+@test-case:
+-----------
+quickestConcat('caution', ['ca', 'ion', 'caut', 'ut']); // -> 2
+quickestConcat('rongbetty', ['wrong', 'bet']); // -> -1
+quickestConcat('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', ['u', 'uu', 'uuu', 'uuuu', 'uuuuu']); // -> 7
+
+*/
