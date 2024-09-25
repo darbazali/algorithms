e7091d783046e9a28eed8a994623173818dae698
Interceptor <darbaz.me@gmail.com>
2024-09-25 16:33:48 +0300

    Add initial implementation and documentation for `befittingBrackets` function in `befitting-brackets.js`

---
+++ src/stack/befitting-brackets.js
@@ -0,0 +1,15 @@
+/* 
+befitting brackets
+------------------
+Write a function, befittingBrackets, that takes in a string as an argument. 
+The function should return a boolean indicating whether or not the string contains correctly matched brackets.
+
+You may assume the string contains only characters: ( ) [ ] { }
+
+@test-cases:
+------------
+befittingBrackets('(){}[](())'); // -> true
+befittingBrackets('({[]})'); // -> true
+befittingBrackets('[][}'); // -> false
+
+*/
