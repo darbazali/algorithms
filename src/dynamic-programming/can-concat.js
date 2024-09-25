055d0ed437c416b61ddf2dff6137c76cc5ff9a28
Interceptor <darbaz.me@gmail.com>
2024-09-25 12:33:41 +0300

    Add memoization to `canConcat` function to improve performance

--- src/dynamic-programming/can-concat.js
+++ src/dynamic-programming/can-concat.js
@@ -12,18 +12,21 @@ canConcat("oneisnone", ["on", "e", "is"]); // -> false
 
 */
 
-const canConcat = (s = "", words = []) => {
+const canConcat = (s = "", words = [], memo = {}) => {
+  if (s in memo) return memo[s]
   if (s === "") return true
 
   for (let word of words) {
     if (s.startsWith(word)) {
       const suffix = s.slice(word.length)
-      if (canConcat(suffix, words)) {
+      if (canConcat(suffix, words, memo)) {
+        memo[s] = true
         return true
       }
     }
   }
 
+  memo[s] = false
   return false
 }
 
