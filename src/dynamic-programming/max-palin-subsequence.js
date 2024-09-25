182f5d8dc65fa1a6fddeaf2fbdab892e18090d3f
Interceptor <darbaz.me@gmail.com>
2024-09-25 10:36:21 +0300

    Add initial implementation and description for maxPalinSubsequence function in dynamic programming module

---
+++ src/dynamic-programming/max-palin-subsequence.js
@@ -0,0 +1,13 @@
+/* 
+max palin subsequence
+---------------------
+Write a function, maxPalinSubsequence, that takes in a string as an argument. 
+The function should return the length of the longest subsequence of the string that is also a palindrome.
+
+A subsequence of a string can be created by deleting any characters of the string, while maintaining the relative order of characters.
+
+@test-case:
+----------
+maxPalinSubsequence("luwxult"); // -> 5
+
+*/
