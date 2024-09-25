edf24e52752eecfa618b0ffa9bb3b189e38f6d8a
Interceptor <darbaz.me@gmail.com>
2024-09-25 12:27:27 +0300

    Add initial implementation and test cases for `canConcat` function in dynamic programming module

---
+++ src/dynamic-programming/can-concat.js
@@ -0,0 +1,13 @@
+/* 
+can concat
+----------
+Write a function, canConcat, that takes in a string and an array of words as arguments. 
+The function should return boolean indicating whether or not it is possible to concatenate words of the array together to form the string.
+
+You may reuse words of the array as many times as needed.
+
+@test-case:
+canConcat("oneisnone", ["one", "none", "is"]); // -> true
+canConcat("oneisnone", ["on", "e", "is"]); // -> false
+
+*/
