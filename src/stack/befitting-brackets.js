50b88fea884eb82d5c2abec443fed5c6a028f7af
Interceptor <darbaz.me@gmail.com>
2024-09-25 16:39:39 +0300

    Implement `befittingBrackets` function to validate bracket sequences and add test cases

--- src/stack/befitting-brackets.js
+++ src/stack/befitting-brackets.js
@@ -13,3 +13,31 @@ befittingBrackets('({[]})'); // -> true
 befittingBrackets('[][}'); // -> false
 
 */
+
+const befittingBrackets = (str) => {
+  const stack = []
+
+  const brackets = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  }
+
+  for (let char of str) {
+    if (char in brackets) {
+      stack.push(brackets[char])
+    } else {
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+
+  return stack.length === 0
+}
+
+console.log(befittingBrackets("(){}[](())")) // -> true
+console.log(befittingBrackets("({[]})")) // -> true
+console.log(befittingBrackets("[][}")) // -> false
