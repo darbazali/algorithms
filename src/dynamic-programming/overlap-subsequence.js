97e103550edcf1d949fe62d0ed1a7aed4e096039
Interceptor <darbaz.me@gmail.com>
2024-09-25 11:02:20 +0300

    Add problem statement and test case for overlapSubsequence function in overlap-subsequence.js

---
+++ src/dynamic-programming/overlap-subsequence.js
@@ -0,0 +1,13 @@
+/* 
+overlap subsequence
+-------------------
+Write a function, overlapSubsequence, that takes in two strings as arguments. 
+The function should return the length of the longest overlapping subsequence.
+A subsequence of a string can be created by deleting any characters of the string, 
+while maintaining the relative order of characters.
+
+@test-cases:
+-----------
+overlapSubsequence("dogs", "daogt"); // -> 3
+
+*/
