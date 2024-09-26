86ee592cc2dbdc17a5eac25cca35320775e97dc7
Interceptor <darbaz.me@gmail.com>
2024-09-26 14:33:11 +0300

    Add subsets function to generate all possible subsets of an array

---
+++ src/exhaustive-recursion/subsets.js
@@ -0,0 +1,23 @@
+/* 
+subsets
+-------
+Write a function, subsets, that takes in an array as an argument. 
+The function should return a 2D array where each subarray represents one of the possible subsets of the array.
+The elements within the subsets and the subsets themselves may be returned in any order.
+You may assume that the input array contains unique elements.
+
+@test-cases:
+subsets(['a', 'b']); // ->
+ [ 
+   [], 
+   [ 'b' ], 
+   [ 'a' ], 
+   [ 'a', 'b' ] 
+ ]
+
+subsets([]); // ->
+ [ 
+   []
+ ]
+
+*/
