ec829eb2641ecdc3607a1f6984ac913340bf69b0
Interceptor <darbaz.me@gmail.com>
2024-09-26 20:13:14 +0300

    Add permutations function with documentation and test cases
    - Implemented the `permutations` function to generate all possible permutations of an input array.
    - Included detailed documentation and example test cases.

---
+++ src/exhaustive-recursion/permutations.js
@@ -0,0 +1,21 @@
+/* 
+permutations
+------------
+Write a function, permutations, that takes in an array an argument. 
+The function should return a 2D array where each subarray represents one of the possible permutations of the array.
+The subarrays may be returned in any order.
+You may assume that the input array contains unique elements.
+
+@test-cases:
+------------
+permutations(['a', 'b', 'c']); // -> 
+ [ 
+   [ 'a', 'b', 'c' ], 
+   [ 'b', 'a', 'c' ], 
+   [ 'b', 'c', 'a' ], 
+   [ 'a', 'c', 'b' ], 
+   [ 'c', 'a', 'b' ], 
+   [ 'c', 'b', 'a' ] 
+ ] 
+
+*/
