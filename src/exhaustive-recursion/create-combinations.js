5cfe1e6d84de147d4bc55007ce4a022924f934f0
Interceptor <darbaz.me@gmail.com>
2024-09-29 12:39:58 +0300

    Add documentation for createCombinations function in create-combinations.js

---
+++ src/exhaustive-recursion/create-combinations.js
@@ -0,0 +1,18 @@
+/* 
+create combinations
+-------------------
+Write a function, createCombinations, that takes in an array and a length as arguments. 
+The function should return a 2D array representing all of the combinations of the specified length.
+The items within the combinations and the combinations themselves may be returned in any order.
+You may assume that the input array contains unique elements and 1 <= k <= items.length.
+
+@test-cases:
+------------
+createCombinations(["a", "b", "c"], 2); // ->
+ [
+   [ 'a', 'b' ],
+   [ 'a', 'c' ],
+   [ 'b', 'c' ]
+ ]
+
+*/
