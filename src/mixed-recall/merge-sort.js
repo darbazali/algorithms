38e984dc13ed97bf72467f08e5c79bc5861f0a0d
Interceptor <darbaz.me@gmail.com>
2024-09-30 22:18:58 +0300

    feat: create merge-sort.js

---
+++ src/mixed-recall/merge-sort.js
@@ -0,0 +1,15 @@
+/* 
+merge sort
+----------
+Write a function, mergeSort, that takes in an array of numbers as an argument. 
+The function should return a new array containing elements of the original array sorted in ascending order. 
+
+@test-cases:
+------------
+const numbers = [10, 4, 42, 5, 8, 100, 5, 6, 12, 40];
+mergeSort(numbers); // -> [ 4, 5, 5, 6, 8, 10, 12, 40, 42, 100 ] 
+
+const numbers = [7, -30, -4, -1, 12, 0, 20];
+mergeSort(numbers); // -> [ -30, -4, -1, 0, 7, 12, 20 ] 
+
+*/
