4aeeb81ea46c806a3dc26b7edc2b9d176e07367a
Interceptor <darbaz.me@gmail.com>
2024-10-02 20:35:13 +0300

    anagrams solution

--- src/arrays-and-strings/anagrams.js
+++ src/arrays-and-strings/anagrams.js
@@ -11,3 +11,32 @@ anagrams('restful', 'fluster'); // -> true
 anagrams('cats', 'tocs'); // -> false
 
 */
+
+const anagrams = (s1, s2) => {
+  const count = {}
+
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0
+    }
+
+    count[char] += 1
+  }
+
+  for (let char of s2) {
+    if (char in count) {
+      count[char] -= 1
+    } else {
+      return false
+    }
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) return false
+  }
+
+  return true
+}
+
+console.log(anagrams("restful", "fluster")) // -> true
+console.log(anagrams("cats", "tocs")) // -> false
