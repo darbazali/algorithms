95f4610c5a3e52b680c4115ec64eeec9ba9f8c3a
Interceptor <darbaz.me@gmail.com>
2024-10-02 20:32:56 +0300

    anagram problem

---
+++ src/arrays-and-strings/anagrams.js
@@ -0,0 +1,13 @@
+/* 
+anagrams
+--------
+Write a function, anagrams, that takes in two strings as arguments. 
+The function should return a boolean indicating whether or not the strings are anagrams. 
+Anagrams are strings that contain the same characters, but in any order.
+
+@test-cases:
+------------
+anagrams('restful', 'fluster'); // -> true
+anagrams('cats', 'tocs'); // -> false
+
+*/
