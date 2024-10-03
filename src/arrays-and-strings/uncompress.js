d463ddd479f40e561809af41ffa1e01f1303db28
Interceptor <darbaz.me@gmail.com>
2024-10-03 21:21:07 +0300

    Add `uncompress` function to decode compressed strings with tests

--- src/arrays-and-strings/uncompress.js
+++ src/arrays-and-strings/uncompress.js
@@ -24,3 +24,27 @@ const repeatString = (str, n) => {
 
   return result.join("")
 }
+
+const uncompress = (str = "") => {
+  let result = ""
+  let i = 0
+  let j = 0
+
+  while (j < str.length) {
+    const char = str[j]
+    if (char.match(/[0-9]/gi)) {
+      j += 1
+    } else {
+      const num = Number(str.slice(i, j))
+      result += repeatString(char, num)
+      j += 1
+      i = j
+    }
+  }
+
+  return result
+}
+
+console.log(uncompress("2c3a1t")) // -> 'ccaaat'
+console.log(uncompress("4s2b")) // -> 'ssssbb'
+console.log(uncompress("3n12e2z")) // -> 'nnneeeeeeeeeeeezz'
