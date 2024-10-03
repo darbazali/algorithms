b11253a02019cb9f6eaa7f425ce386018f59f137
Interceptor <darbaz.me@gmail.com>
2024-10-03 20:47:49 +0300

    Add implementation for pairSum function with test cases

--- src/arrays-and-strings/pair-sum.js
+++ src/arrays-and-strings/pair-sum.js
@@ -16,3 +16,19 @@ pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
 pairSum([9, 9], 18); // -> [0, 1]
 
 */
+
+const pairSum = (numbers, targetSum) => {
+  const prev = {}
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    const num = numbers[i]
+    const complement = targetSum - num
+    if (complement in prev) return [prev[complement], i]
+
+    prev[num] = i
+  }
+}
+
+console.log(pairSum([3, 2, 5, 4, 1], 8)) // -> [0, 2]
+console.log(pairSum([4, 7, 9, 2, 5, 1], 5)) // -> [0, 5]
+console.log(pairSum([9, 9], 18)) // -> [0, 1]
