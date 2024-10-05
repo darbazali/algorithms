517e13e3b4e4395ea678f04c9f3cabfbae866edd
Interceptor <darbaz.me@gmail.com>
2024-10-05 13:59:20 +0300

    Add `fiveSort` function to reorder array by moving all 5s to the end

--- src/arrays-and-strings/five-sort.js
+++ src/arrays-and-strings/five-sort.js
@@ -15,3 +15,25 @@ fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]); // -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5]
 fiveSort([5, 5, 5, 1, 1, 1, 4]); // -> [4, 1, 1, 1, 5, 5, 5] 
 
 */
+
+const fiveSort = (nums = []) => {
+  let i = 0
+  let j = nums.length - 1
+
+  while (i <= j) {
+    if (nums[j] === 5) {
+      j -= 1
+    } else if (nums[i] === 5) {
+      ;[nums[i], nums[j]] = [nums[j], nums[i]]
+      i += 1
+    } else {
+      i += 1
+    }
+  }
+
+  return nums
+}
+
+console.log(fiveSort([12, 5, 1, 5, 12, 7]))
+console.log(fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]))
+console.log(fiveSort([5, 5, 5, 1, 1, 1, 4]))
