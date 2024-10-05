15a907517d3cd37fabb7008dc91c24936486b3ad
Interceptor <darbaz.me@gmail.com>
2024-10-05 16:45:41 +0300

    Add recursive function to calculate sum of string lengths in an array

--- recursion/sum-of-lengths.js
+++ recursion/sum-of-lengths.js
@@ -14,3 +14,8 @@ sumOfLengths(["", " ", "  ", "   ", "    ", "     "]); // -> 15
 sumOfLengths(["0", "313", "1234567890"]); // -> 14 
 
 */
+
+const sumOfLengths = (strings = []) => {
+  if (strings.length === 0) return 0
+  return strings[0].length + sumOfLengths(strings.slice(1))
+}
