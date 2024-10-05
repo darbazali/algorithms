008961a48de8b2bacdf910f956fc5eef13985c4a
Interceptor <darbaz.me@gmail.com>
2024-10-05 16:25:37 +0300

    Remove console.log statements from sum-numbers-recursive.js

--- recursion/sum-numbers-recursive.js
+++ recursion/sum-numbers-recursive.js
@@ -16,7 +16,3 @@ const sumNumbersRecursive = (numbers = []) => {
   if (numbers.length === 0) return 0
   return numbers[0] + sumNumbersRecursive(numbers.slice(1))
 }
-
-console.log(sumNumbersRecursive([5, 2, 9, 10])) // -> 26
-console.log(sumNumbersRecursive([1, -1, 1, -1, 1, -1, 1])) // -> 1
-console.log(sumNumbersRecursive([])) // -> 0
