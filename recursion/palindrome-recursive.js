630bd337b03cc4df53c6eccd39846a0b20baccc0
Interceptor <darbaz.me@gmail.com>
2024-10-05 20:30:59 +0300

    Add recursive palindrome function with test cases

--- recursion/palindrome-recursive.js
+++ recursion/palindrome-recursive.js
@@ -12,3 +12,13 @@ palindrome("kayak"); // -> true
 palindrome(""); // -> true
 
 */
+
+const palindrome = (s = "") => {
+  if (s.length <= 1) return true
+  if (s[0] !== s[s.length - 1]) return false
+  return palindrome(s.slice(1, -1))
+}
+
+console.log(palindrome("pop")) // -> true
+console.log(palindrome("kayak")) // -> true
+console.log(palindrome("")) // -> true
