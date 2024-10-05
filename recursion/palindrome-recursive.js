0a4baeda324f9a300e39718b192be5bde18940fc
Interceptor <darbaz.me@gmail.com>
2024-10-05 20:29:05 +0300

    Add documentation for recursive palindrome function implementation

---
+++ recursion/palindrome-recursive.js
@@ -0,0 +1,14 @@
+/* 
+palindrome recursive
+--------------------
+Write a function, palindrome, that takes in a string and returns a boolean indicating whether or not the string is the same forwards and backwards.
+
+You must solve this recursively.
+
+@test-cases:
+------------
+palindrome("pop"); // -> true
+palindrome("kayak"); // -> true
+palindrome(""); // -> true
+
+*/
