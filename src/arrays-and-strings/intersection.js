4ebf5851c28fcb7cbe5ba6095e9e13c93d4cebf0
Interceptor <darbaz.me@gmail.com>
2024-10-05 13:37:05 +0300

    Add `intersection` function to find common elements in two arrays

---
+++ src/arrays-and-strings/intersection.js
@@ -0,0 +1,15 @@
+/* 
+intersection
+------------
+Write a function, intersection, that takes in two arrays, a,b, as arguments. 
+The function should return a new array containing elements that are in both of the two arrays.
+
+You may assume that each input array does not contain duplicate elements.
+
+@test-cases:
+------------
+intersection([4,2,1,6], [3,6,9,2,10]) // -> [2,6]
+intersection([2,4,6], [4,2]) // -> [2,4]
+intersection([0,1,2], [10,11]) // -> []
+
+*/
