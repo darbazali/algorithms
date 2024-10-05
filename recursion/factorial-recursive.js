4fd1657963e4c6cdc220a769b6f40ce705d0dbd8
Interceptor <darbaz.me@gmail.com>
2024-10-05 16:37:06 +0300

    Add recursive factorial function implementation with test cases

---
+++ recursion/factorial-recursive.js
@@ -0,0 +1,19 @@
+/* 
+factorial
+Write a function, factorial, that takes in a number n and returns the factorial of that number. 
+The factorial of n is the product of all the positive numbers less than or equal to n. 
+You must solve this recursively.
+
+For example, the factorial of 6 is:
+
+6 * 5 * 4 * 3 * 2 * 1 = 720
+
+@test-cases:
+------------
+factorial(3); // -> 6
+factorial(6); // -> 720
+factorial(18); // -> 6402373705728000
+factorial(1); // -> 1
+factorial(0); // -> 1
+
+*/
