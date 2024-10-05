472fb8e2da4f8692d07d055c35f6ac07ef2b342e
Interceptor <darbaz.me@gmail.com>
2024-10-05 16:23:46 +0300

    Add documentation for sumNumbersRecursive function with test cases

---
+++ recursion/sum-numbers-recursive.js
@@ -0,0 +1,13 @@
+/* 
+sum numbers recursive
+---------------------
+Write a function sumNumbersRecursive that takes in an array of numbers and returns the sum of all the numbers in the array. 
+All elements will be integers. Solve this recursively.
+
+@test-cases:
+------------
+sumNumbersRecursive([5, 2, 9, 10]); // -> 26
+sumNumbersRecursive([1, -1, 1, -1, 1, -1, 1]); // -> 1
+sumNumbersRecursive([]); // -> 0
+
+*/
