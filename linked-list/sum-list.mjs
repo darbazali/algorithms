86a5bd4b21a9c771e138deb79ed0fd1a7b95677b
Interceptor <darbaz.me@gmail.com>
2024-10-06 22:02:10 +0300

    Add sumList function to calculate sum of linked list values

--- linked-list/sum-list.mjs
+++ linked-list/sum-list.mjs
@@ -22,3 +22,30 @@ d.next = e;
 sumList(a); // 19
 
 */
+
+import Node from "./linked-list-node.mjs"
+
+const sumList = (head) => {
+  let sum = 0
+  let current = head
+
+  while (current !== null) {
+    sum += current.val
+    current = current.next
+  }
+
+  return sum
+}
+
+const a = new Node(2)
+const b = new Node(8)
+const c = new Node(3)
+const d = new Node(-1)
+const e = new Node(7)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+console.log(sumList(a)) // -> 19
