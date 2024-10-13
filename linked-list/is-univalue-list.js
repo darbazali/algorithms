f1bf86ed913a283f662c7510e2f71622757aeb83
Interceptor <darbaz.me@gmail.com>
2024-10-13 22:01:48 +0300

    Add implementation for checking if a linked list is univalue

--- linked-list/is-univalue-list.js
+++ linked-list/is-univalue-list.js
@@ -18,3 +18,14 @@ b.next = c;
 isUnivalueList(a); // true
 
 */
+
+const isUnivalueList = (head) => {
+  let current = head
+
+  while (current !== null) {
+    if (current.val !== head.val) return false
+    current = current.next
+  }
+
+  return true
+}
