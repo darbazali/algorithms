10457847511d37b1eb05643d3430c4e68bd4392e
Interceptor <darbaz.me@gmail.com>
2024-10-13 21:45:25 +0300

    Add zipperLists function to merge two linked lists in alternating order

--- linked-list/zipper-lists.js
+++ linked-list/zipper-lists.js
@@ -27,3 +27,28 @@ zipperLists(a, x);
  a -> x -> b -> y -> c -> z
 
 */
+
+const zipperLists = (head1, head2) => {
+  let current1 = head1.next
+  let current2 = head2
+  let tail = head1
+  let count = 0
+
+  while (current1 !== null && current2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = current2
+      current2 = current2.next
+    } else {
+      tail.next = current1
+      current1 = current1.next
+    }
+
+    tail = tail.next
+    count += 1
+  }
+
+  if (current1 !== null) tail.next = current1
+  if (current2 !== null) tail.next = current2
+
+  return head1
+}
