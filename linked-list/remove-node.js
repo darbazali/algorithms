37e308e069132a9deb24d299b9292835836d539d
Interceptor <darbaz.me@gmail.com>
2024-10-14 12:25:28 +0300

    Add function to remove a node from a linked list by value

--- linked-list/remove-node.js
+++ linked-list/remove-node.js
@@ -31,3 +31,22 @@ removeNode(a, "c");
  a -> b -> d -> e -> f
 
 */
+
+const removeNode = (head, targetVal) => {
+  if (head.val === targetVal) return head.next
+
+  let current = head
+  let prev = null
+
+  while (current !== null) {
+    if (current.val === targetVal) {
+      prev.next = current.next
+      break
+    }
+
+    prev = current
+    current = current.next
+  }
+
+  return head
+}
